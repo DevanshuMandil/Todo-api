8597b98731b8a098dfa8d848913466ec827d20c5
DevanshuMandil <devanshmandil@gmail.com>
2018-02-12 02:49:39 +0530

    ADD post to todos

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 var express=require('express');
+var bodyParser = require('body-parser');
+
 var app=express();
 var PORT=process.env.PORT || 3000;
 
-var todos=[{
-	id:1,
-	description:'wake up in morning',
-	completed: false
-},{
-	id:2,
-	description: 'get ready for college',
-	completed: false
-},{
-	id:3,
-	description:'do the breakfast',
-	completed: true
-}];
+var todos=[];
+var todoNextId=1;
+app.use(bodyParser.json());
 
 //GET /todos
 
@@ -46,6 +38,18 @@ app.get('/todos/:id',function(req,res){
 	{
 		res.status(404).send();
 	}
+});
+
+// POST /todos
+
+app.post('/todos',function(req, res){
+	var body=req.body;
+	body.id=todoNextId;
+	todoNextId++;
+	todos.push(body);
+
+	// console.log('description:'+ body.description);
+	 res.json(todos);
 });
 
 app.get('/',function(req ,res){
